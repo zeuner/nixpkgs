41f3a1559173b4f36113effadc1a987b8be875de
Isidor Zeuner <nix@quidecco.pl>
2023-09-13 15:08:54 +0200

    mpfi: fetch through tag again in case of update

--- pkgs/development/libraries/mpfi/default.nix
+++ pkgs/development/libraries/mpfi/default.nix
@@ -1,19 +1,21 @@
 {lib, stdenv, fetchFromGitLab, autoreconfHook, texinfo, mpfr}:
+
 stdenv.mkDerivation rec {
   pname = "mpfi";
   version = "1.5.4";
 
-  src = fetchFromGitLab {
+  src = fetchFromGitLab ({
     domain = "gitlab.inria.fr";
     owner = "mpfi";
     repo = "mpfi";
 
-    # Apparently there is an upstream off-by-one-commit error in tagging
-    #rev = version;
-    rev = "feab26bc54529417af983950ddbffb3a4c334d4f";
+    rev = version;
 
     sha256 = "sha256-aj/QmJ38ifsW36JFQcbp55aIQRvOpiqLHwEh/aFXsgo=";
-  };
+  } // lib.optionalAttrs (version == "1.5.4") {
+    # Apparently there is an upstream off-by-one-commit error in tagging
+    rev = "feab26bc54529417af983950ddbffb3a4c334d4f";
+  });
 
   sourceRoot = "source/mpfi";
 
